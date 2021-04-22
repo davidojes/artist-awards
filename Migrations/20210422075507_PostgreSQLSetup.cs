9b4cfffee75f568602d1d69cdee5b62c29b070d4
davidojes <ojesekho@sheridancollege.ca>
2021-04-22 04:19:37 -0400

    Add postgres database migrations

---
+++ Migrations/20210422075507_PostgreSQLSetup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArtistAwards.Migrations
+{
+    public partial class PostgreSQLSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
