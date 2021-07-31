ae4c4ee6b31ee7fa662317cd2cf74e6fc8758632
davidojes <ojesekho@sheridancollege.ca>
2021-07-31 01:03:29 -0400

    Change URLs that are used by the app

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace DotNetAPI
             .ConfigureWebHostDefaults(webBuilder =>
             {
               webBuilder.UseStartup<Startup>();
+              webBuilder.UseUrls("http://localhost:5004;https://localhost:5005");
             });
   }
 }
